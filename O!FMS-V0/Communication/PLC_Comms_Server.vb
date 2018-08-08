a55b6839e1e43e3370b567f483b7eef82682b6ef
Ethen <ebot14rocks@gmail.com>
2018-08-08 16:49:28 -0400

    Added the file to be read in an IDE

--- O!FMS-V0/Communication/PLC_Comms_Server.vb
+++ O!FMS-V0/Communication/PLC_Comms_Server.vb
@@ -1,10 +1,11 @@
-﻿Imports System.Net
+﻿﻿Imports System.Net
 Imports System.Net.Sockets
 Imports System
 Imports System.Collections
 Imports System.Security.Cryptography
-Imports myOPC
-Imports OPCAutomation
+'Imports myOPC
+'Imports OPCAutomation
+Imports EasyModbus
 
 
 
@@ -56,165 +57,287 @@ Public Class PLC_Comms_Server
     Public Shared PLC_Used_Lev_Blue
     Public Shared PLC_Match_Timer
     Public Shared PLC_Match_Mode
-
+    Public Shared PLC_RedPen_Ref
+    Public Shared PLC_BluePen_Ref
+    Public Shared PLC_BlueScaleOwned
+    Public Shared PLC_RedScaleOwned
+    Public Shared PLC_BlueSWOwned
+    Public Shared PLC_RedSWOwned
     'Data Sent from FMS Software to PLC
     Public Shared Game_Data
+    Public Shared PLC_Game_Data
     Public Shared Match_Start
     Public Shared Match_Stop
     Public Shared PLC_Reset
 
+    'Alliance Station Lights
+
+    'RED Alliance
+    Public Shared StnRed1Red
+    Public Shared StnRed2Red
+    Public Shared StnRed3Red
+
+    Public Shared StnRed1Amb
+    Public Shared StnRed2Amb
+    Public Shared StnRed3Amb
+
+
+    'BLUE Alliance
+    Public Shared StnBlue1Blue
+    Public Shared StnBlue2Blue
+    Public Shared StnBlue3Blue
+
+    Public Shared StnBlue1Amb
+    Public Shared StnBlue2Amb
+    Public Shared StnBlue3Amb
+
+    'Field Control Lights
+    Public Shared FieldGreen
+    Public Shared FieldBlue
+    Public Shared FieldRed
+    Public Shared FieldAmber
+
+
+    Public Shared Sub Modbus_Main(ByVal args() As String)
+        Dim modbusClient As ModbusClient = New ModbusClient("10.0.0.7", 502)
+        'Ip-Address and Port of Modbus-TCP-Server
+        modbusClient.Connect()
+        'Connect to Server
+
+        Dim readCoils() As Boolean = modbusClient.ReadCoils(0, 50)
+        'Read 50 Coils from Server, starting with address 0
+
+        Dim readHoldingRegisters() As Integer = modbusClient.ReadHoldingRegisters(0, 20)
+        'Read 20 Holding Registers from Server, starting with Address 0
+
+        ' Output
+        Dim i As Integer = 0
+        Do While (i < readCoils.Length)
+            Console.WriteLine(("Value of Coil " + ((42 + (i + 1)) + (" " + readCoils(i).ToString))))
+
+
+            PLC_Estop_Field = readCoils(0)
+            PLC_Estop_Red1 = readCoils(1)
+            PLC_Estop_Red2 = readCoils(2)
+            PLC_Estop_Red3 = readCoils(3)
+            PLC_Estop_Blue1 = readCoils(4)
+            PLC_Estop_Blue2 = readCoils(5)
+            PLC_Estop_Blue3 = readCoils(6)
+
+
+            PLC_Used_Boost_Red = readCoils(7)
+            PLC_Used_Force_Red = readCoils(8)
+            PLC_Used_Lev_Red = readCoils(9)
+            PLC_Used_Boost_Blue = readCoils(10)
+            PLC_Used_Force_Blue = readCoils(11)
+            PLC_Used_Lev_Blue = readCoils(12)
+
+
+            PLC_BlueScaleOwned = readCoils(40)
+            PLC_BlueSWOwned = readCoils(41)
+            PLC_RedSWOwned = readCoils(42)
+            PLC_RedScaleOwned = readCoils(43)
+
+
+
+            i = (i + 1)
+        Loop
+
+
+        Dim j As Integer = 0
+        Do While (j < readHoldingRegisters.Length)
+            Console.WriteLine(("Value of HoldingRegister " + ((j + 1) + (" " + readHoldingRegisters(j).ToString))))
+
+            PLC_Match_Timer = readHoldingRegisters(0)
+            PLC_Match_Mode = readHoldingRegisters(1)
+            PLC_RedScore = readHoldingRegisters(2)
+            PLC_BlueScore = readHoldingRegisters(3)
+            PLC_RedPen_Ref = readHoldingRegisters(5)
+            PLC_BluePen_Ref = readHoldingRegisters(6)
+
+
+            j = (j + 1)
+        Loop
+
+
+
+        If Match_Start = True Then
+            modbusClient.WriteSingleCoil(13, True)
+            'Writes value True to Coil Address 13
+        End If
+
+        If Match_Stop = True Then
+            modbusClient.WriteSingleCoil(14, True)
+        End If
+
+        If PLC_Reset = True Then
+            modbusClient.WriteSingleCoil(15, True)
+        End If
+
+        modbusClient.WriteSingleRegister(4, True)
+
+
+        'modbusClient.WriteMultipleCoils(16, New Boolean() {True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True})
+        'Write Coils starting with Address 16
 
 
 
 
-
-
-    Public Sub New(ByVal addressFamily As AddressFamily, ByVal socketType As SocketType, ByVal protocolType As ProtocolType)
-
-
-
-        Dim bindAddress As IPAddress, parsedAddress As IPAddress
-
-        bindAddress = IPAddress.Any
-        bindAddress = IPAddress.IPv6Any
-
-
-        Try
-            'set to the FMS Computer Ip
-            parsedAddress = IPAddress.Parse("10.0.0.5")
-            parsedAddress = IPAddress.Parse("fe80::2ff:abcd:1234%3")
-        Catch err As FormatException
-            Console.WriteLine("Invalid IP Address", err.Message)
-        End Try
-
-
-        Dim bindEndPoint As IPEndPoint = New IPEndPoint(bindAddress, 5000)
-        Dim plcSocket = Nothing
-
-        Try
-            plcSocket = New Socket(bindAddress.AddressFamily, socketType.Dgram, protocolType.Udp)
-            plcSocket.Bind(bindEndPoint)
-        Catch err As SocketException
-            If (Not plcSocket Is Nothing) Then
-                plcSocket.Close()
-            End If
-        End Try
-
-        Dim tcpSocket As Socket
-        Dim resolvedServer As IPHostEntry
-        Dim serverEndPoint As IPEndPoint
-        Dim addr As IPAddress
-
-        tcpSocket = Nothing
-
-        Try
-            resolvedServer = Dns.GetHostEntry("PLC Connection")
-            For Each addr In resolvedServer.AddressList
-                serverEndPoint = New IPEndPoint(addr, 5000)
-                tcpSocket = New Socket(addr.AddressFamily, socketType.Stream, protocolType.Tcp)
-                Try
-                    tcpSocket.Connect(serverEndPoint)
-
-                Catch ex As Exception
-                    If (Not tcpSocket Is Nothing) Then
-                        tcpSocket.Close()
-                    End If
-                    GoTo ContinueLoop
-                End Try
-ContinueLoop:
-            Next
-        Catch ex As Exception
-            Console.WriteLine("Client connection failed: {0}")
-        End Try
-        'Now we communicate with a TCP Socket with the Server'
-        Dim ClientSocket As Socket
-
-        Dim dataBuffer(1024) As Byte 'sample byte'
-
-
-        Try
-            ClientSocket.Send(dataBuffer)
-            ClientSocket.Send() 'Add the packets to the send method'
-        Catch ex As Exception
-            Console.WriteLine("Send Failed {0}")
-        End Try
-
-        Dim destAddress As IPAddress = IPAddress.Parse("10.0.0.5")
-        Dim destEndPoint As IPEndPoint = New IPEndPoint(destAddress, 8000)
-        Dim udpSocket As Socket
-        Dim message() As Byte = System.Text.Encoding.ASCII.GetBytes("Hello World")
-
-        udpSocket = New Socket(destAddress.AddressFamily, socketType.Dgram, protocolType.Udp)
-
-        Try
-            udpSocket.SendTo(message, destEndPoint)
-        Catch ex As Exception
-            Console.WriteLine("Send To Failed {0}")
-        End Try
-        'Receive Data'
-        Dim receiveBuffer(1024) As Byte
-        Dim senderEndPoint As IPEndPoint = New IPEndPoint(bindAddress.AddressFamily, 0)
-        Dim castSenderEndPoint As EndPoint = CType(senderEndPoint, EndPoint)
-        Dim rc As Integer
-
-        udpSocket = New Socket(bindAddress.AddressFamily, socketType.Dgram, protocolType.Udp)
-        Try
-            udpSocket.Bind(bindEndPoint)
-            udpSocket.ReceiveFrom(receiveBuffer, castSenderEndPoint)
-            senderEndPoint = CType(castSenderEndPoint, IPEndPoint)
-            Console.WriteLine("Received {0} byte from {1}", rc, senderEndPoint.ToString())
-        Catch ex As Exception
-            Console.WriteLine("Error Occurred: {0}")
-        Finally
-            udpSocket.Close()
-        End Try
-
-        Dim requestBuffer(1024) As Byte
-
-        Try
-            tcpSocket.Send(requestBuffer)
-            tcpSocket.Shutdown(SocketShutdown.Send)
-
-            Do While True
-                rc = tcpSocket.Receive(requestBuffer)
-                'may need to change the shutdown data rate'
-                If (rc > 1) Then
-                ElseIf (rc = 1) Then
-                    Exit Do
-                End If
-            Loop
-
-        Catch ex As Exception
-            Console.WriteLine("An Error Occurred: {0}")
-        End Try
+        modbusClient.Disconnect()
+        'Disconnect from Server
+        Console.Write("Press any key to continue . . . ")
+        Console.ReadKey(True)
     End Sub
+
+
+
+
+    '    Public Sub New(ByVal addressFamily As AddressFamily, ByVal socketType As SocketType, ByVal protocolType As ProtocolType)
+
+
+
+    '        Dim bindAddress As IPAddress, parsedAddress As IPAddress
+
+    '        bindAddress = IPAddress.Any
+    '        bindAddress = IPAddress.IPv6Any
+
+
+    '        Try
+    '            'set to the FMS Computer Ip
+    '            parsedAddress = IPAddress.Parse("10.0.0.5")
+    '            parsedAddress = IPAddress.Parse("fe80::2ff:abcd:1234%3")
+    '        Catch err As FormatException
+    '            Console.WriteLine("Invalid IP Address", err.Message)
+    '        End Try
+
+
+    '        Dim bindEndPoint As IPEndPoint = New IPEndPoint(bindAddress, 5000)
+    '        Dim plcSocket = Nothing
+
+    '        Try
+    '            plcSocket = New Socket(bindAddress.AddressFamily, socketType.Dgram, protocolType.Udp)
+    '            plcSocket.Bind(bindEndPoint)
+    '        Catch err As SocketException
+    '            If (Not plcSocket Is Nothing) Then
+    '                plcSocket.Close()
+    '            End If
+    '        End Try
+
+    '        Dim tcpSocket As Socket
+    '        Dim resolvedServer As IPHostEntry
+    '        Dim serverEndPoint As IPEndPoint
+    '        Dim addr As IPAddress
+
+    '        tcpSocket = Nothing
+
+    '        Try
+    '            resolvedServer = Dns.GetHostEntry("PLC Connection")
+    '            For Each addr In resolvedServer.AddressList
+    '                serverEndPoint = New IPEndPoint(addr, 5000)
+    '                tcpSocket = New Socket(addr.AddressFamily, socketType.Stream, protocolType.Tcp)
+    '                Try
+    '                    tcpSocket.Connect(serverEndPoint)
+
+    '                Catch ex As Exception
+    '                    If (Not tcpSocket Is Nothing) Then
+    '                        tcpSocket.Close()
+    '                    End If
+    '                    GoTo ContinueLoop
+    '                End Try
+    'ContinueLoop:
+    '            Next
+    '        Catch ex As Exception
+    '            Console.WriteLine("Client connection failed: {0}")
+    '        End Try
+    '        'Now we communicate with a TCP Socket with the Server'
+    '        Dim ClientSocket As Socket
+
+    '        Dim dataBuffer(1024) As Byte 'sample byte'
+
+
+    '        Try
+    '            ClientSocket.Send(dataBuffer)
+    '            'ClientSocket.Send() 'Add the packets to the send method'
+    '        Catch ex As Exception
+    '            Console.WriteLine("Send Failed {0}")
+    '        End Try
+
+    '        Dim destAddress As IPAddress = IPAddress.Parse("10.0.0.5")
+    '        Dim destEndPoint As IPEndPoint = New IPEndPoint(destAddress, 8000)
+    '        Dim udpSocket As Socket
+    '        Dim message() As Byte = System.Text.Encoding.ASCII.GetBytes("Hello World")
+
+    '        udpSocket = New Socket(destAddress.AddressFamily, socketType.Dgram, protocolType.Udp)
+
+    '        Try
+    '            udpSocket.SendTo(message, destEndPoint)
+    '        Catch ex As Exception
+    '            Console.WriteLine("Send To Failed {0}")
+    '        End Try
+    '        'Receive Data'
+    '        Dim receiveBuffer(1024) As Byte
+    '        Dim senderEndPoint As IPEndPoint = New IPEndPoint(bindAddress.AddressFamily, 0)
+    '        Dim castSenderEndPoint As EndPoint = CType(senderEndPoint, EndPoint)
+    '        Dim rc As Integer
+
+    '        udpSocket = New Socket(bindAddress.AddressFamily, socketType.Dgram, protocolType.Udp)
+    '        Try
+    '            udpSocket.Bind(bindEndPoint)
+    '            udpSocket.ReceiveFrom(receiveBuffer, castSenderEndPoint)
+    '            senderEndPoint = CType(castSenderEndPoint, IPEndPoint)
+    '            Console.WriteLine("Received {0} byte from {1}", rc, senderEndPoint.ToString())
+    '        Catch ex As Exception
+    '            Console.WriteLine("Error Occurred: {0}")
+    '        Finally
+    '            udpSocket.Close()
+    '        End Try
+
+    '        Dim requestBuffer(1024) As Byte
+
+    '        Try
+    '            tcpSocket.Send(requestBuffer)
+    '            tcpSocket.Shutdown(SocketShutdown.Send)
+
+    '            Do While True
+    '                rc = tcpSocket.Receive(requestBuffer)
+    '                'may need to change the shutdown data rate'
+    '                If (rc > 1) Then
+    '                ElseIf (rc = 1) Then
+    '                    Exit Do
+    '                End If
+    '            Loop
+
+    '        Catch ex As Exception
+    '            Console.WriteLine("An Error Occurred: {0}")
+    '        End Try
+    '    End Sub
     'Varibles for the packets'
-    Dim LIGHT_OFF As Byte = "0"
-    Dim LIGHT_ON As Byte = "1"
-    Dim LIGHT_BLINKING As Byte = "S"
-    Dim FIELD_OFF As Byte = "0"
-    Dim FIELD_ON As Byte = "1"
-    Dim FIELD_BLINKING As Byte = "S"
-    Dim LIGHT_MODE As Byte = "L"
-    Dim TEAM_NUM_MODE As Byte = "N"
-    Dim TIME_MODE As Byte = "T"
-    Dim VIEW_MODE As Byte = "V"
-    Dim BYTE_ZERO As Byte = "0"
-    Dim BYTE_CLEAR As Byte = BYTE_ZERO
-    Dim HW_MODE As Byte = "H"
+    'Dim LIGHT_OFF As Byte = "0"
+    'Dim LIGHT_ON As Byte = "1"
+    'Dim LIGHT_BLINKING As Byte = "S"
+    'Dim FIELD_OFF As Byte = "0"
+    'Dim FIELD_ON As Byte = "1"
+    'Dim FIELD_BLINKING As Byte = "S"
+    'Dim LIGHT_MODE As Byte = "L"
+    'Dim TEAM_NUM_MODE As Byte = "N"
+    'Dim TIME_MODE As Byte = "T"
+    'Dim VIEW_MODE As Byte = "V"
+    'Dim BYTE_ZERO As Byte = "0"
+    'Dim BYTE_CLEAR As Byte = BYTE_ZERO
+    'Dim HW_MODE As Byte = "H"
 
-    'Scale byte
-    Dim SCALE_SENSOR1 As Byte = "SS1"
-    Dim SCALE_SENSOR2 As Byte = "SS2"
+    ''Scale byte
+    'Dim SCALE_SENSOR1 As Byte = "SS1"
+    'Dim SCALE_SENSOR2 As Byte = "SS2"
 
-    'Red side Switch
-    Dim RED_SWITCH_SENSOR1 As Byte = "RSS1"
-    Dim RED_SWITCH_SENSOR2 As Byte = "RSS2"
+    ''Red side Switch
+    'Dim RED_SWITCH_SENSOR1 As Byte = "RSS1"
+    'Dim RED_SWITCH_SENSOR2 As Byte = "RSS2"
 
-    'Blue side Switch
-    Dim BLUE_SWITCH_SENSOR2 As Byte = "BSS2"
-    Dim BLUE_SWITCH_SENSOR1 As Byte = "BSS1"
-    Dim NeitherAlliance As Byte = "NA"
+    ''Blue side Switch
+    'Dim BLUE_SWITCH_SENSOR2 As Byte = "BSS2"
+    'Dim BLUE_SWITCH_SENSOR1 As Byte = "BSS1"
+    'Dim NeitherAlliance As Byte = "NA"
 
     'Private Shared RED As Integer = FieldAndRobots.RED
     'Private Shared BLUE As Integer = FieldAndRobots.BLUE
@@ -248,14 +371,14 @@ ContinueLoop:
     '    Return data(9)
     'End Function
 
-    Private Function checkAndFixNum(ByVal initTime As String, ByVal length As Integer) As String
-        If (initTime.Length < length) Then
-            initTime = "0".Concat(initTime)
-            Return Me.checkAndFixNum(initTime, length)
-        End If
+    'Private Function checkAndFixNum(ByVal initTime As String, ByVal length As Integer) As String
+    '    If (initTime.Length < length) Then
+    '        initTime = "0".Concat(initTime)
+    '        Return Me.checkAndFixNum(initTime, length)
+    '    End If
 
-        Return initTime
-    End Function
+    '    Return initTime
+    'End Function
 
 
 
