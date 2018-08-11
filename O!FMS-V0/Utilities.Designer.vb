b10659580e46c91251cddaf17a857ea5e8f7091a
ScottL222 <40001626+ScottL222@users.noreply.github.com>
2018-08-10 21:35:09 -0400

    Add files via upload

---
+++ O!FMS-V0/Utilities.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Utilities
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PLC_Status = New System.Windows.Forms.Label()
+        Me.Red_Alliance_Box = New System.Windows.Forms.Label()
+        Me.Blue_Alliance_Box = New System.Windows.Forms.Label()
+        Me.Red_Vault = New System.Windows.Forms.Label()
+        Me.Blue_Vault = New System.Windows.Forms.Label()
+        Me.Red_ViewMarq = New System.Windows.Forms.Label()
+        Me.Blue_ViewMarq = New System.Windows.Forms.Label()
+        Me.Light_Controller = New System.Windows.Forms.Label()
+        Me.Access_Piont = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FMS = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'PLC_Status
+        '
+        Me.PLC_Status.AutoSize = True
+        Me.PLC_Status.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PLC_Status.Location = New System.Drawing.Point(53, 39)
+        Me.PLC_Status.Name = "PLC_Status"
+        Me.PLC_Status.Size = New System.Drawing.Size(121, 22)
+        Me.PLC_Status.TabIndex = 0
+        Me.PLC_Status.Text = "PLC_Status"
+        '
+        'Red_Alliance_Box
+        '
+        Me.Red_Alliance_Box.AutoSize = True
+        Me.Red_Alliance_Box.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Red_Alliance_Box.Location = New System.Drawing.Point(53, 61)
+        Me.Red_Alliance_Box.Name = "Red_Alliance_Box"
+        Me.Red_Alliance_Box.Size = New System.Drawing.Size(188, 22)
+        Me.Red_Alliance_Box.TabIndex = 1
+        Me.Red_Alliance_Box.Text = "Red_Alliance_Box"
+        '
+        'Blue_Alliance_Box
+        '
+        Me.Blue_Alliance_Box.AutoSize = True
+        Me.Blue_Alliance_Box.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Blue_Alliance_Box.Location = New System.Drawing.Point(53, 83)
+        Me.Blue_Alliance_Box.Name = "Blue_Alliance_Box"
+        Me.Blue_Alliance_Box.Size = New System.Drawing.Size(199, 22)
+        Me.Blue_Alliance_Box.TabIndex = 2
+        Me.Blue_Alliance_Box.Text = "Blue_Alliance_Box"
+        '
+        'Red_Vault
+        '
+        Me.Red_Vault.AutoSize = True
+        Me.Red_Vault.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Red_Vault.Location = New System.Drawing.Point(53, 105)
+        Me.Red_Vault.Name = "Red_Vault"
+        Me.Red_Vault.Size = New System.Drawing.Size(112, 22)
+        Me.Red_Vault.TabIndex = 3
+        Me.Red_Vault.Text = "Red_Vault"
+        '
+        'Blue_Vault
+        '
+        Me.Blue_Vault.AutoSize = True
+        Me.Blue_Vault.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Blue_Vault.Location = New System.Drawing.Point(53, 127)
+        Me.Blue_Vault.Name = "Blue_Vault"
+        Me.Blue_Vault.Size = New System.Drawing.Size(123, 22)
+        Me.Blue_Vault.TabIndex = 4
+        Me.Blue_Vault.Text = "Blue_Vault"
+        '
+        'Red_ViewMarq
+        '
+        Me.Red_ViewMarq.AutoSize = True
+        Me.Red_ViewMarq.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Red_ViewMarq.Location = New System.Drawing.Point(53, 149)
+        Me.Red_ViewMarq.Name = "Red_ViewMarq"
+        Me.Red_ViewMarq.Size = New System.Drawing.Size(153, 22)
+        Me.Red_ViewMarq.TabIndex = 5
+        Me.Red_ViewMarq.Text = "Red_ViewMarq"
+        '
+        'Blue_ViewMarq
+        '
+        Me.Blue_ViewMarq.AutoSize = True
+        Me.Blue_ViewMarq.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Blue_ViewMarq.Location = New System.Drawing.Point(53, 171)
+        Me.Blue_ViewMarq.Name = "Blue_ViewMarq"
+        Me.Blue_ViewMarq.Size = New System.Drawing.Size(164, 22)
+        Me.Blue_ViewMarq.TabIndex = 6
+        Me.Blue_ViewMarq.Text = "Blue_ViewMarq"
+        '
+        'Light_Controller
+        '
+        Me.Light_Controller.AutoSize = True
+        Me.Light_Controller.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Light_Controller.Location = New System.Drawing.Point(53, 193)
+        Me.Light_Controller.Name = "Light_Controller"
+        Me.Light_Controller.Size = New System.Drawing.Size(190, 22)
+        Me.Light_Controller.TabIndex = 7
+        Me.Light_Controller.Text = "Light_Controller"
+        '
+        'Access_Piont
+        '
+        Me.Access_Piont.AutoSize = True
+        Me.Access_Piont.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Access_Piont.Location = New System.Drawing.Point(53, 215)
+        Me.Access_Piont.Name = "Access_Piont"
+        Me.Access_Piont.Size = New System.Drawing.Size(142, 22)
+        Me.Access_Piont.TabIndex = 8
+        Me.Access_Piont.Text = "Access_Piont"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(425, 436)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FMS
+        '
+        Me.FMS.AutoSize = True
+        Me.FMS.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FMS.Location = New System.Drawing.Point(53, 9)
+        Me.FMS.Name = "FMS"
+        Me.FMS.Size = New System.Drawing.Size(72, 22)
+        Me.FMS.TabIndex = 10
+        Me.FMS.Text = "FMS IP"
+        '
+        'Utilities
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(512, 480)
+        Me.Controls.Add(Me.FMS)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Access_Piont)
+        Me.Controls.Add(Me.Light_Controller)
+        Me.Controls.Add(Me.Blue_ViewMarq)
+        Me.Controls.Add(Me.Red_ViewMarq)
+        Me.Controls.Add(Me.Blue_Vault)
+        Me.Controls.Add(Me.Red_Vault)
+        Me.Controls.Add(Me.Blue_Alliance_Box)
+        Me.Controls.Add(Me.Red_Alliance_Box)
+        Me.Controls.Add(Me.PLC_Status)
+        Me.Name = "Utilities"
+        Me.Text = "Utilities"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PLC_Status As System.Windows.Forms.Label
+    Friend WithEvents Red_Alliance_Box As System.Windows.Forms.Label
+    Friend WithEvents Blue_Alliance_Box As System.Windows.Forms.Label
+    Friend WithEvents Red_Vault As System.Windows.Forms.Label
+    Friend WithEvents Blue_Vault As System.Windows.Forms.Label
+    Friend WithEvents Red_ViewMarq As System.Windows.Forms.Label
+    Friend WithEvents Blue_ViewMarq As System.Windows.Forms.Label
+    Friend WithEvents Light_Controller As System.Windows.Forms.Label
+    Friend WithEvents Access_Piont As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FMS As System.Windows.Forms.Label
+End Class
---
+++ O!FMS-V0/Utilities.vb
@@ -0,0 +1,115 @@
+﻿Public Class Utilities
+
+    Dim AdminNetwork As String = "10.0.0."
+    Dim FMS_PC As String = "5"
+    Dim PLC As String = "7"
+    Dim Red_Box As String = "10"
+    Dim Blue_Box As String = "11"
+    Dim Red_Vault_Box As String = "8"
+    Dim Blue_Vault_Box As String = "9"
+    Dim Access_Piont_1 As String
+    Dim red_ViewMarq_1 As String
+    Dim blue_viewmarq_1 As String
+    Dim light_controller_1 As String = "13"
+
+    Private Sub Utilities_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If My.Computer.Network.Ping(AdminNetwork & FMS_PC, 1000) Then
+            FMS.ForeColor = Color.LimeGreen
+        Else : FMS.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & PLC, 1000) Then
+            PLC_Status.ForeColor = Color.LimeGreen
+        Else : PLC_Status.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Red_Box, 1000) Then
+            Red_Alliance_Box.ForeColor = Color.LimeGreen
+        Else : Red_Alliance_Box.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Blue_Box, 1000) Then
+            Blue_Alliance_Box.ForeColor = Color.LimeGreen
+        Else : Blue_Alliance_Box.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Red_Vault_Box, 1000) Then
+            Red_Vault.ForeColor = Color.LimeGreen
+        Else : Red_Vault.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Blue_Vault_Box, 1000) Then
+            Blue_Vault.ForeColor = Color.LimeGreen
+        Else : Blue_Vault.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Access_Piont_1, 1000) Then
+            Access_Piont.ForeColor = Color.LimeGreen
+        Else : Access_Piont.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & red_ViewMarq_1, 1000) Then
+            Red_ViewMarq.ForeColor = Color.LimeGreen
+        Else : Red_ViewMarq.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & blue_viewmarq_1, 1000) Then
+            Blue_ViewMarq.ForeColor = Color.LimeGreen
+        Else : Blue_ViewMarq.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & light_controller_1, 1000) Then
+            Light_Controller.ForeColor = Color.LimeGreen
+        Else : Light_Controller.ForeColor = Color.Red
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If My.Computer.Network.Ping(AdminNetwork & PLC, 1000) Then
+            PLC_Status.ForeColor = Color.LimeGreen
+        Else : PLC_Status.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Red_Box, 1000) Then
+            Red_Alliance_Box.ForeColor = Color.LimeGreen
+        Else : Red_Alliance_Box.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Blue_Box, 1000) Then
+            Blue_Alliance_Box.ForeColor = Color.LimeGreen
+        Else : Blue_Alliance_Box.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Red_Vault_Box, 1000) Then
+            Red_Vault.ForeColor = Color.LimeGreen
+        Else : Red_Vault.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Blue_Vault_Box, 1000) Then
+            Blue_Vault.ForeColor = Color.LimeGreen
+        Else : Blue_Vault.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & Access_Piont_1, 1000) Then
+            Access_Piont.ForeColor = Color.LimeGreen
+        Else : Access_Piont.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & red_ViewMarq_1, 1000) Then
+            Red_ViewMarq.ForeColor = Color.LimeGreen
+        Else : Red_ViewMarq.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & blue_viewmarq_1, 1000) Then
+            Blue_ViewMarq.ForeColor = Color.LimeGreen
+        Else : Blue_ViewMarq.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork & light_controller_1, 1000) Then
+            Light_Controller.ForeColor = Color.LimeGreen
+        Else : Light_Controller.ForeColor = Color.Red
+        End If
+    End Sub
+End Class
