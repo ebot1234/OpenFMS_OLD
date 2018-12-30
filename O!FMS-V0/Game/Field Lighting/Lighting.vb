8c986c57e624b8c46c64fdf74b8b672d42bb288f
ebot1234 <ebot14rocks@gmail.com>
2018-12-29 21:33:39 -0500

    added global varibles

--- O!FMS-V0/Game/Field Lighting/Lighting.vb
+++ O!FMS-V0/Game/Field Lighting/Lighting.vb
@@ -1,17 +1,20 @@
 ﻿Imports System.Net.Sockets
 Imports System.Text
 Imports System.Net
-Imports O_FMS_V0.RandomString
 'This class is for interating with the led controllers for the 2018 FRC game FIRST POWERUP'
 
 Public Class Lighting
 
+    Public Shared address As String = "10.0.100.23"
+    Public Shared port As Integer = 80
+    Public Shared socket As Socket
+
+    'Sends the mode string to the led controller through a UDP socket'
     Public Shared Sub setMode(mode As String)
         'Creates the socket for communicating with the led controller'
-        Dim socket As Socket = New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+        socket = New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
         'Address of the led controller set as an EndPoint with the port'
-        Dim serverAddress As IPAddress = IPAddress.Parse("10.0.100.23")
-        Dim endPoint As New IPEndPoint(serverAddress, 80)
+        Dim endPoint As New IPEndPoint(address, port)
         'Byte buffer for sending data'
         Dim send_buffer As [Byte]() = Encoding.ASCII.GetBytes(mode)
         'Sends data to the led controller through the socket'
