cb73025e161d8a9ca6ad4120308844140e96393a
ebot1234 <ebot14rocks@gmail.com>
2018-12-29 21:30:09 -0500

    added some documentation

--- O!FMS-V0/Game/Field Lighting/Lighting.vb
+++ O!FMS-V0/Game/Field Lighting/Lighting.vb
@@ -9,14 +9,14 @@ Public Class Lighting
     Public Shared Sub setMode(mode As String)
         'Creates the socket for communicating with the led controller'
         Dim socket As Socket = New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
-
+        'Address of the led controller set as an EndPoint with the port'
         Dim serverAddress As IPAddress = IPAddress.Parse("10.0.100.23")
         Dim endPoint As New IPEndPoint(serverAddress, 80)
-
+        'Byte buffer for sending data'
         Dim send_buffer As [Byte]() = Encoding.ASCII.GetBytes(mode)
-
+        'Sends data to the led controller through the socket'
         socket.SendTo(send_buffer, endPoint)
-
+        'Thread sleeps 100ms for a break'
         Threading.Thread.Sleep(100)
     End Sub
 End Class
