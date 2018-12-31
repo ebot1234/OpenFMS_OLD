47cab4e2354b61786fe2f8783aaab360e10d51e0
ebot1234 <ebot14rocks@gmail.com>
2018-12-31 15:50:44 -0500

    added a way to interface with the two switch led controllers

--- O!FMS-V0/Game/Field Lighting/Lighting.vb
+++ O!FMS-V0/Game/Field Lighting/Lighting.vb
@@ -19,4 +19,35 @@ Public Class Lighting
         'Thread sleeps 100ms for a break'
         Threading.Thread.Sleep(100)
     End Sub
+
+    Public Shared Sub setModeBlueSwitch(mode As String)
+        'Creates the socket for communicating with the led controller'
+        Dim Socket = New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+        'Address of the led controller set as an EndPoint with the port'
+        Dim serverAddress As IPAddress = IPAddress.Parse("10.0.100.24")
+        Dim port As Integer = 80
+        Dim endPoint As New IPEndPoint(serverAddress, port)
+        'Byte buffer for sending data'
+        Dim send_buffer As [Byte]() = Encoding.ASCII.GetBytes(mode)
+        'Sends data to the led controller through the socket'
+        Socket.SendTo(send_buffer, endPoint)
+        'Thread sleeps 100ms for a break'
+        Threading.Thread.Sleep(100)
+
+    End Sub
+
+    Public Shared Sub setModeRedSwitch(mode As String)
+        'Creates the socket for communicating with the led controller'
+        Dim Socket = New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+        'Address of the led controller set as an EndPoint with the port'
+        Dim serverAddress As IPAddress = IPAddress.Parse("10.0.100.25")
+        Dim port As Integer = 80
+        Dim endPoint As New IPEndPoint(serverAddress, port)
+        'Byte buffer for sending data'
+        Dim send_buffer As [Byte]() = Encoding.ASCII.GetBytes(mode)
+        'Sends data to the led controller through the socket'
+        Socket.SendTo(send_buffer, endPoint)
+        'Thread sleeps 100ms for a break'
+        Threading.Thread.Sleep(100)
+    End Sub
 End Class
