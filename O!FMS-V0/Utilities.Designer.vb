fb8e1849fbbea2699e32cc92c7b706dd3defafc3
ebot1234 <ebot14rocks@gmail.com>
2019-03-21 16:56:04 -0400

    worked on the hardware checker

--- O!FMS-V0/Utilities.Designer.vb
+++ O!FMS-V0/Utilities.Designer.vb
@@ -25,12 +25,11 @@ Partial Class Utilities
         Me.PLC_Status = New System.Windows.Forms.Label()
         Me.Red_Alliance_Box = New System.Windows.Forms.Label()
         Me.Blue_Alliance_Box = New System.Windows.Forms.Label()
-        Me.Red_Vault = New System.Windows.Forms.Label()
-        Me.Blue_Vault = New System.Windows.Forms.Label()
+        Me.LightController2 = New System.Windows.Forms.Label()
         Me.Red_ViewMarq = New System.Windows.Forms.Label()
         Me.Blue_ViewMarq = New System.Windows.Forms.Label()
-        Me.Light_Controller = New System.Windows.Forms.Label()
-        Me.Access_Piont = New System.Windows.Forms.Label()
+        Me.Light_Controller1 = New System.Windows.Forms.Label()
+        Me.Access_PointLbl = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.FMS = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -42,21 +41,21 @@ Partial Class Utilities
         '
         Me.PLC_Status.AutoSize = True
         Me.PLC_Status.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.PLC_Status.Location = New System.Drawing.Point(281, 162)
+        Me.PLC_Status.Location = New System.Drawing.Point(321, 162)
         Me.PLC_Status.Name = "PLC_Status"
-        Me.PLC_Status.Size = New System.Drawing.Size(105, 24)
+        Me.PLC_Status.Size = New System.Drawing.Size(45, 24)
         Me.PLC_Status.TabIndex = 0
-        Me.PLC_Status.Text = "PLC_Status"
+        Me.PLC_Status.Text = "PLC"
         '
         'Red_Alliance_Box
         '
         Me.Red_Alliance_Box.AutoSize = True
         Me.Red_Alliance_Box.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Red_Alliance_Box.Location = New System.Drawing.Point(521, 162)
+        Me.Red_Alliance_Box.Location = New System.Drawing.Point(593, 162)
         Me.Red_Alliance_Box.Name = "Red_Alliance_Box"
-        Me.Red_Alliance_Box.Size = New System.Drawing.Size(165, 24)
+        Me.Red_Alliance_Box.Size = New System.Drawing.Size(88, 24)
         Me.Red_Alliance_Box.TabIndex = 1
-        Me.Red_Alliance_Box.Text = "Red_Alliance_Box"
+        Me.Red_Alliance_Box.Text = "Red SCC"
         '
         'Blue_Alliance_Box
         '
@@ -64,29 +63,19 @@ Partial Class Utilities
         Me.Blue_Alliance_Box.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Blue_Alliance_Box.Location = New System.Drawing.Point(3, 162)
         Me.Blue_Alliance_Box.Name = "Blue_Alliance_Box"
-        Me.Blue_Alliance_Box.Size = New System.Drawing.Size(168, 24)
+        Me.Blue_Alliance_Box.Size = New System.Drawing.Size(91, 24)
         Me.Blue_Alliance_Box.TabIndex = 2
-        Me.Blue_Alliance_Box.Text = "Blue_Alliance_Box"
+        Me.Blue_Alliance_Box.Text = "Blue SCC"
         '
-        'Red_Vault
+        'LightController2
         '
-        Me.Red_Vault.AutoSize = True
-        Me.Red_Vault.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Red_Vault.Location = New System.Drawing.Point(589, 209)
-        Me.Red_Vault.Name = "Red_Vault"
-        Me.Red_Vault.Size = New System.Drawing.Size(97, 24)
-        Me.Red_Vault.TabIndex = 3
-        Me.Red_Vault.Text = "Red_Vault"
-        '
-        'Blue_Vault
-        '
-        Me.Blue_Vault.AutoSize = True
-        Me.Blue_Vault.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Blue_Vault.Location = New System.Drawing.Point(3, 209)
-        Me.Blue_Vault.Name = "Blue_Vault"
-        Me.Blue_Vault.Size = New System.Drawing.Size(100, 24)
-        Me.Blue_Vault.TabIndex = 4
-        Me.Blue_Vault.Text = "Blue_Vault"
+        Me.LightController2.AutoSize = True
+        Me.LightController2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LightController2.Location = New System.Drawing.Point(475, 209)
+        Me.LightController2.Name = "LightController2"
+        Me.LightController2.Size = New System.Drawing.Size(206, 24)
+        Me.LightController2.TabIndex = 3
+        Me.LightController2.Text = "Rocket Led Controller 2"
         '
         'Red_ViewMarq
         '
@@ -94,9 +83,9 @@ Partial Class Utilities
         Me.Red_ViewMarq.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Red_ViewMarq.Location = New System.Drawing.Point(546, 110)
         Me.Red_ViewMarq.Name = "Red_ViewMarq"
-        Me.Red_ViewMarq.Size = New System.Drawing.Size(140, 24)
+        Me.Red_ViewMarq.Size = New System.Drawing.Size(142, 24)
         Me.Red_ViewMarq.TabIndex = 5
-        Me.Red_ViewMarq.Text = "Red_ViewMarq"
+        Me.Red_ViewMarq.Text = "Red Team Sign"
         '
         'Blue_ViewMarq
         '
@@ -104,30 +93,30 @@ Partial Class Utilities
         Me.Blue_ViewMarq.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Blue_ViewMarq.Location = New System.Drawing.Point(3, 110)
         Me.Blue_ViewMarq.Name = "Blue_ViewMarq"
-        Me.Blue_ViewMarq.Size = New System.Drawing.Size(143, 24)
+        Me.Blue_ViewMarq.Size = New System.Drawing.Size(145, 24)
         Me.Blue_ViewMarq.TabIndex = 6
-        Me.Blue_ViewMarq.Text = "Blue_ViewMarq"
+        Me.Blue_ViewMarq.Text = "Blue Team Sign"
         '
-        'Light_Controller
+        'Light_Controller1
         '
-        Me.Light_Controller.AutoSize = True
-        Me.Light_Controller.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Light_Controller.Location = New System.Drawing.Point(269, 209)
-        Me.Light_Controller.Name = "Light_Controller"
-        Me.Light_Controller.Size = New System.Drawing.Size(141, 24)
-        Me.Light_Controller.TabIndex = 7
-        Me.Light_Controller.Text = "Light_Controller"
+        Me.Light_Controller1.AutoSize = True
+        Me.Light_Controller1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Light_Controller1.Location = New System.Drawing.Point(3, 209)
+        Me.Light_Controller1.Name = "Light_Controller1"
+        Me.Light_Controller1.Size = New System.Drawing.Size(206, 24)
+        Me.Light_Controller1.TabIndex = 7
+        Me.Light_Controller1.Text = "Rocket Led Controller 1"
         '
-        'Access_Piont
+        'Access_PointLbl
         '
-        Me.Access_Piont.AutoSize = True
-        Me.Access_Piont.BackColor = System.Drawing.Color.AliceBlue
-        Me.Access_Piont.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Access_Piont.Location = New System.Drawing.Point(269, 110)
-        Me.Access_Piont.Name = "Access_Piont"
-        Me.Access_Piont.Size = New System.Drawing.Size(124, 24)
-        Me.Access_Piont.TabIndex = 8
-        Me.Access_Piont.Text = "Access_Point"
+        Me.Access_PointLbl.AutoSize = True
+        Me.Access_PointLbl.BackColor = System.Drawing.Color.AliceBlue
+        Me.Access_PointLbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Access_PointLbl.Location = New System.Drawing.Point(279, 110)
+        Me.Access_PointLbl.Name = "Access_PointLbl"
+        Me.Access_PointLbl.Size = New System.Drawing.Size(124, 24)
+        Me.Access_PointLbl.TabIndex = 8
+        Me.Access_PointLbl.Text = "Access_Point"
         '
         'Button1
         '
@@ -142,7 +131,7 @@ Partial Class Utilities
         '
         Me.FMS.AutoSize = True
         Me.FMS.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.FMS.Location = New System.Drawing.Point(311, 62)
+        Me.FMS.Location = New System.Drawing.Point(321, 64)
         Me.FMS.Name = "FMS"
         Me.FMS.Size = New System.Drawing.Size(55, 24)
         Me.FMS.TabIndex = 10
@@ -161,12 +150,11 @@ Partial Class Utilities
         'Panel1
         '
         Me.Panel1.Controls.Add(Me.FMS)
-        Me.Panel1.Controls.Add(Me.Red_Vault)
-        Me.Panel1.Controls.Add(Me.Blue_Vault)
-        Me.Panel1.Controls.Add(Me.Access_Piont)
+        Me.Panel1.Controls.Add(Me.LightController2)
+        Me.Panel1.Controls.Add(Me.Access_PointLbl)
         Me.Panel1.Controls.Add(Me.Red_Alliance_Box)
         Me.Panel1.Controls.Add(Me.Blue_Alliance_Box)
-        Me.Panel1.Controls.Add(Me.Light_Controller)
+        Me.Panel1.Controls.Add(Me.Light_Controller1)
         Me.Panel1.Controls.Add(Me.Red_ViewMarq)
         Me.Panel1.Controls.Add(Me.Blue_ViewMarq)
         Me.Panel1.Controls.Add(Me.PLC_Status)
@@ -180,7 +168,7 @@ Partial Class Utilities
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.AliceBlue
-        Me.ClientSize = New System.Drawing.Size(687, 480)
+        Me.ClientSize = New System.Drawing.Size(695, 480)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
@@ -195,12 +183,11 @@ Partial Class Utilities
     Friend WithEvents PLC_Status As System.Windows.Forms.Label
     Friend WithEvents Red_Alliance_Box As System.Windows.Forms.Label
     Friend WithEvents Blue_Alliance_Box As System.Windows.Forms.Label
-    Friend WithEvents Red_Vault As System.Windows.Forms.Label
-    Friend WithEvents Blue_Vault As System.Windows.Forms.Label
+    Friend WithEvents LightController2 As System.Windows.Forms.Label
     Friend WithEvents Red_ViewMarq As System.Windows.Forms.Label
     Friend WithEvents Blue_ViewMarq As System.Windows.Forms.Label
-    Friend WithEvents Light_Controller As System.Windows.Forms.Label
-    Friend WithEvents Access_Piont As System.Windows.Forms.Label
+    Friend WithEvents Light_Controller1 As System.Windows.Forms.Label
+    Friend WithEvents Access_PointLbl As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents FMS As System.Windows.Forms.Label
     Friend WithEvents Label1 As Label
--- O!FMS-V0/Utilities.vb
+++ O!FMS-V0/Utilities.vb
@@ -3,67 +3,80 @@
 Public Class Utilities
 
     Dim AdminNetwork As String = "10.0.0."
-    Dim FMS_PC As String = "5"
     Dim PLC As String = "7"
+    Dim FMS_PC As String = "5"
     Dim Red_Box As String = "10"
     Dim Blue_Box As String = "11"
-    Dim Red_Vault_Box As String = "8"
-    Dim Blue_Vault_Box As String = "9"
-    Dim Access_Piont_1 As String = "15"
-    Dim red_ViewMarq_1 As String = "16"
-    Dim blue_viewmarq_1 As String = "17"
-    Dim light_controller_1 As String = "13"
-
-    Private Sub Utilities_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
-
-    End Sub
+    Dim Access_Point As String = "15"
+    Dim Red_team_sign As String = "16"
+    Dim Blue_team_sign As String = "17"
+    'Year Specifics'
+    Dim Rocket_led_controller_1 As String = "13"
+    Dim Rocket_led_controller_2 As String = "14"
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If My.Computer.Network.Ping(AdminNetwork & PLC, 1000) Then
+        checkBasicHardware()
+        checkPLCHardware()
+        checkFieldSpecificHardware()
+    End Sub
+
+    Private Sub checkBasicHardware()
+        If My.Computer.Network.Ping(AdminNetwork + Access_Point, 1000) Then
+            Access_PointLbl.ForeColor = Color.LimeGreen
+        Else
+            Access_PointLbl.ForeColor = Color.Red
+        End If
+
+        If My.Computer.Network.Ping(AdminNetwork + FMS_PC) Then
+            FMS.ForeColor = Color.LimeGreen
+        Else
+            FMS.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub checkPLCHardware()
+        If My.Computer.Network.Ping(AdminNetwork + PLC, 1000) Then
             PLC_Status.ForeColor = Color.LimeGreen
-        Else : PLC_Status.ForeColor = Color.Red
+        Else
+            PLC_Status.ForeColor = Color.Red
         End If
 
-        If My.Computer.Network.Ping(AdminNetwork & Red_Box, 1000) Then
+        If My.Computer.Network.Ping(AdminNetwork + Red_Box, 1000) Then
             Red_Alliance_Box.ForeColor = Color.LimeGreen
-        Else : Red_Alliance_Box.ForeColor = Color.Red
+        Else
+            Red_Alliance_Box.ForeColor = Color.Red
         End If
 
-        If My.Computer.Network.Ping(AdminNetwork & Blue_Box, 1000) Then
+        If My.Computer.Network.Ping(AdminNetwork + Blue_Box, 1000) Then
             Blue_Alliance_Box.ForeColor = Color.LimeGreen
-        Else : Blue_Alliance_Box.ForeColor = Color.Red
+        Else
+            Blue_Alliance_Box.ForeColor = Color.Red
         End If
 
-        If My.Computer.Network.Ping(AdminNetwork & Red_Vault_Box, 1000) Then
-            Red_Vault.ForeColor = Color.LimeGreen
-        Else : Red_Vault.ForeColor = Color.Red
-        End If
-
-        If My.Computer.Network.Ping(AdminNetwork & Blue_Vault_Box, 1000) Then
-            Blue_Vault.ForeColor = Color.LimeGreen
-        Else : Blue_Vault.ForeColor = Color.Red
-        End If
-
-        If My.Computer.Network.Ping(AdminNetwork & Access_Piont_1, 1000) Then
-            Access_Piont.ForeColor = Color.LimeGreen
-        Else : Access_Piont.ForeColor = Color.Red
-        End If
-
-        If My.Computer.Network.Ping(AdminNetwork & red_ViewMarq_1, 1000) Then
+        If My.Computer.Network.Ping(AdminNetwork + Red_team_sign, 1000) Then
             Red_ViewMarq.ForeColor = Color.LimeGreen
-        Else : Red_ViewMarq.ForeColor = Color.Red
+        Else
+            Red_ViewMarq.ForeColor = Color.Red
         End If
 
-        If My.Computer.Network.Ping(AdminNetwork & blue_viewmarq_1, 1000) Then
+        If My.Computer.Network.Ping(AdminNetwork + Blue_team_sign, 1000) Then
             Blue_ViewMarq.ForeColor = Color.LimeGreen
-        Else : Blue_ViewMarq.ForeColor = Color.Red
+        Else
+            Blue_ViewMarq.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub checkFieldSpecificHardware()
+        If My.Computer.Network.Ping(AdminNetwork + Rocket_led_controller_1, 1000) Then
+            Light_Controller1.ForeColor = Color.LimeGreen
+        Else
+            Light_Controller1.ForeColor = Color.Red
         End If
 
-        If My.Computer.Network.Ping(AdminNetwork & light_controller_1, 1000) Then
-            Light_Controller.ForeColor = Color.LimeGreen
-        Else : Light_Controller.ForeColor = Color.Red
+        If My.Computer.Network.Ping(AdminNetwork + Rocket_led_controller_2, 1000) Then
+            LightController2.ForeColor = Color.LimeGreen
+        Else
+            LightController2.ForeColor = Color.Red
         End If
     End Sub
 
