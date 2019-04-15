33bea238e5fabe1dadf2ad559435ffc405ff0c2d
ebot1234 <ebot14rocks@gmail.com>
2019-04-15 18:11:36 -0400

    fixed the switch configuration for teams with team numbers up to 4 digits and fixed the new line carraige return for VB.Net

--- O!FMS-V0/OFMS/Field/Switch.vb
+++ O!FMS-V0/OFMS/Field/Switch.vb
@@ -40,14 +40,14 @@ Public Class Switch
     End Function
 
     Public Shared Function configureTeamEthernet(red1 As String, red2 As String, red3 As String, blue1 As String, blue2 As String, blue3 As String)
-        Switch.mutex.ReleaseMutex()
 
-        'Determines what new VLANs are needed
+        'Determines what new VLANs are needed'
         Dim oldTeamVlans = getTeamVlans()
         If oldTeamVlans Is Nothing Then
             MessageBox.Show("No VLANs Avalible")
         End If
 
+        'Sets the Vlans command for each team in the match'
         replaceVlan(red1, Red1VLAN)
         replaceVlan(red2, Red2VLAN)
         replaceVlan(red3, Red3VLAN)
@@ -57,12 +57,18 @@ Public Class Switch
 
         Dim removeTeamVlansCommand = ""
         'Builds the team vlan removal command'
-        removeTeamVlansCommand = String.Format("interface Vlan{0}\nno ip address\nno access-list 1{1}\n" +
-                                                       "interface Vlan{2}\nno ip address\nno access-list 1{3}\n" +
-                                                       "interface Vlan{4}\nno ip address\nno access-list 1{5}\n" +
-                                                       "interface Vlan{6}\nno ip address\nno access-list 1{7}\n" +
-                                                       "interface Vlan{8}\nno ip address\nno access-list 1{9}\n" +
-                                                       "interface Vlan{10}\nno ip address\nno access-list 1{11}\n", Red1VLAN, Red1VLAN,
+        removeTeamVlansCommand = String.Format("interface Vlan{0}" & vbNewLine & "no ip address" & vbNewLine &
+                                                        "no access-list 1{1}" & vbNewLine &
+                                                       "interface Vlan{2}" & vbNewLine & "no ip address" & vbNewLine &
+                                                       "no access-list 1{3}" & vbNewLine &
+                                                       "interface Vlan{4}" & vbNewLine & "no ip address" & vbNewLine &
+                                                       "no access-list 1{5}" & vbNewLine &
+                                                       "interface Vlan{6}" & vbNewLine & "no ip address" & vbNewLine &
+                                                       "no access-list 1{7}" & vbNewLine &
+                                                       "interface Vlan{8}" & vbNewLine & "no ip address" & vbNewLine &
+                                                       "no access-list 1{9}" & vbNewLine &
+                                                       "interface Vlan{10}" & vbNewLine & "no ip address" & vbNewLine & "
+                                                       no access-list 1{11}" & vbNewLine, Red1VLAN, Red1VLAN,
                                                         Red2VLAN, Red2VLAN, Red3VLAN, Red3VLAN, Blue1VLAN, Blue1VLAN, Blue2VLAN, Blue2VLAN,
                                                         Blue3VLAN, Blue3VLAN)
 
@@ -79,7 +85,19 @@ Public Class Switch
     End Function
 
     Public Shared Function replaceVlan(teamNumber As String, vlan As Integer)
-
+        'Determines the team number ip address format for use with the Access Point command'
+        Select Case (teamNumber.Length)
+            Case 4
+                teamNumber = teamNumber.Substring(0, 2) + "." + teamNumber.Substring(2, 2)
+            Case 3
+                teamNumber = teamNumber.Substring(0, 1) + "." + teamNumber.Substring(1, 2)
+            Case 2
+                teamNumber = "0." + teamNumber.Substring(0, 2)
+            Case 1
+                teamNumber = "0.0" + teamNumber.Substring(0, 1)
+            Case Else
+                MessageBox.Show(String.Format("Team: {0} doesn't work to short or long", teamNumber))
+        End Select
 
         Dim oldTeamVlans = getTeamVlans()
 
@@ -88,19 +106,18 @@ Public Class Switch
         End If
         'add vlan checking before command?'
         addTeamVlanCommand = String.Format(
-                                     "ip dhcp excluded-address 10.{0}.{1}.100 10.{2}.{3}.100\n" +
-                                                 "no ip dhcp pool dhcp{4}\n" +
-                                                 "ip dhcp pool dhcp{5}\n" +
-                                                 "network 10.{6}.{7}.0 255.255.255.0\n" +
-                                                 "default-router 10.{8}.{9}.61\n" +
-                                                 "lease 7\n" +
-                                                 "no access-list 1{10}\n" +
-                                                 "access-list 1{11} permit ip 10.{12}.{13}.0 0.0.0.255 host {14}\n" +
-                                                 "access-list 1{15} permit udp any eq bootpc any eq bootps\n" +
-                                                 "interface Vlan{16}\nip address 10.{17}.{18}.61 255.255.255.0\n",
-                                      teamNumber.Substring(0, 2), teamNumber.Substring(2, 2), teamNumber.Substring(0, 2), teamNumber.Substring(2, 2), vlan, vlan,
-                                     teamNumber.Substring(0, 2), teamNumber.Substring(2, 2), teamNumber.Substring(0, 2), teamNumber.Substring(2, 2), vlan, vlan, teamNumber.Substring(0, 2), teamNumber.Substring(2, 2),
-                                     ServerIpAddress, vlan, vlan, teamNumber.Substring(0, 2), teamNumber.Substring(2, 2))
+                                     "ip dhcp excluded-address 10.{0}.100 10.{1}.100" & vbNewLine &
+                                                 "no ip dhcp pool dhcp{2}" & vbNewLine &
+                                                 "ip dhcp pool dhcp{3}" & vbNewLine &
+                                                 "network 10.{4}.0 255.255.255.0" & vbNewLine &
+                                                 "default-router 10.{5}.61" & vbNewLine &
+                                                 "lease 7" & vbNewLine &
+                                                 "no access-list 1{6}" & vbNewLine &
+                                                 "access-list 1{7} permit ip 10.{8}.0 0.0.0.255 host {9}" & vbNewLine &
+                                                 "access-list 1{10} permit udp any eq bootpc any eq bootps" & vbNewLine &
+                                                 "interface Vlan{10}" & vbNewLine & "ip address 10.{11}.61 255.255.255.0" & vbNewLine,
+                                      teamNumber, teamNumber, vlan, vlan, teamNumber, teamNumber, vlan, vlan, teamNumber,
+                                      ServerIpAddress, vlan, vlan, teamNumber)
 
         Return addTeamVlanCommand
     End Function
@@ -114,7 +131,7 @@ Public Class Switch
         'Checks if the network stream can transport data'
         If NetworkStream.CanWrite Then
             'Format the string for username and password'
-            Dim commandString As String = String.Format("{0}\nenable\n{1}\nterminal length 0\n{2}exit\n", Switch.password, Switch.password, command)
+            Dim commandString As String = String.Format("{0}" & vbNewLine & "enable" & vbNewLine & "{1}" & vbNewLine & "terminal length 0" & vbNewLine & "{2}exit" & vbNewLine, Switch.password, Switch.password, command)
             Dim networkByte As [Byte]() = Encoding.ASCII.GetBytes(commandString)
             NetworkStream.Write(networkByte, 0, networkByte.Length)
         End If
@@ -131,6 +148,6 @@ Public Class Switch
     End Function
 
     Public Shared Function runConfigCommand(command As String)
-        Return runCommand(String.Format("config terminal\n{0}send\ncopy running-config startup-config\n\n", command))
+        Return runCommand(String.Format("config terminal" & vbNewLine & "{0}send" & vbNewLine & "copy running-config startup-config" & vbNewLine & vbNewLine, command))
     End Function
 End Class
