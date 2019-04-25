44820862b4c97ade24fc9c2631b4ab67d55a2f2b
Ethen <ebot14rocks@gmail.com>
2019-04-25 10:06:48 -0400

    changed the way the HTTP request get the Authorization string

--- O!FMS-V0/TBA/Tba.vb
+++ O!FMS-V0/TBA/Tba.vb
@@ -3,20 +3,22 @@ Imports System.Net
 Imports System.IO
 
 Public Class Tba
+    
+    public shared TBA_Auth_Key as string = "Lat8J29zc3UrMOy8X4TSnreTqitAE9oMUvOqmpgXgPR0B6k4k96kh7UCiMjEy2Kg"
 
     Public Function getTeamId(number As String)
         Return String.Format("frc{0}", number)
     End Function
 
     'gets data from the Blue Alliance via JSON and HTTP GET request'
-    Public Sub getJsonRequest(url As String, command As String)
+    Public Sub getTbaTeam(team As String, command As String)
         Dim request As HttpWebRequest
         Dim response As HttpWebResponse = Nothing
         Dim reader As StreamReader
 
-        Dim requestAddress As String = String.Format("https://www.thebluealliance.com/api/v3/{0}", url)
+        Dim requestAddress As String = String.Format("https://www.thebluealliance.com/api/v3/team/{0}", team)
         request = DirectCast(WebRequest.Create(requestAddress), HttpWebRequest)
-        request.Headers.Set("X-TBA-Auth-Key", "Lat8J29zc3UrMOy8X4TSnreTqitAE9oMUvOqmpgXgPR0B6k4k96kh7UCiMjEy2Kg")
+        request.Headers.Set("X-TBA-Auth-Key", TBA_Auth_Key)
 
         response = DirectCast(request.GetResponse(), HttpWebResponse)
         reader = New StreamReader(response.GetResponseStream())
