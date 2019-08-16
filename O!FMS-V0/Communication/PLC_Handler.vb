02fff32ee62b93d23bf669259cc5bcc095858d1f
ebot1234 <ebot14rocks@gmail.com>
2019-08-16 16:49:19 -0400

    added some automated scoring
    added foul and tech foul scoring, hatch and cargo scoring. As well as red rocket placement

--- O!FMS-V0/Communication/PLC_Handler.vb
+++ O!FMS-V0/Communication/PLC_Handler.vb
@@ -76,7 +76,52 @@ Public Class PLC_Handler
     Public Shared BlueRocket2Light
     Public Shared red_hatch_count
     Public Shared red_cargo_count
-
+    Public Shared blue_hatch_count
+    Public Shared blue_cargo_count
+    Public Shared red_rocket_rp_count
+    Public Shared blue_rocket_rp_count
+    Public Shared red_hab_rp_count
+    Public Shared blue_hab_rp_count
+    Public Shared red_rocket_near_bay1
+    Public Shared red_rocket_near_bay2
+    Public Shared red_rocket_near_bay3
+    Public Shared red_rocket_near_bay4
+    Public Shared red_rocket_near_bay5
+    Public Shared red_rocket_near_bay6
+    Public Shared red_rocket_far_bay1
+    Public Shared red_rocket_far_bay2
+    Public Shared red_rocket_far_bay3
+    Public Shared red_rocket_far_bay4
+    Public Shared red_rocket_far_bay5
+    Public Shared red_rocket_far_bay6
+    Public Shared blue_rocket_near_bay1
+    Public Shared blue_rocket_near_bay2
+    Public Shared blue_rocket_near_bay3
+    Public Shared blue_rocket_near_bay4
+    Public Shared blue_rocket_near_bay5
+    Public Shared blue_rocket_near_bay6
+    Public Shared blue_rocket_far_bay1
+    Public Shared blue_rocket_far_bay2
+    Public Shared blue_rocket_far_bay3
+    Public Shared blue_rocket_far_bay4
+    Public Shared blue_rocket_far_bay5
+    Public Shared blue_rocket_far_bay6
+    Public Shared red_cs_bay1
+    Public Shared red_cs_bay2
+    Public Shared red_cs_bay3
+    Public Shared red_cs_bay4
+    Public Shared red_cs_bay5
+    Public Shared red_cs_bay6
+    Public Shared red_cs_bay7
+    Public Shared red_cs_bay8
+    Public Shared blue_cs_bay1
+    Public Shared blue_cs_bay2
+    Public Shared blue_cs_bay3
+    Public Shared blue_cs_bay4
+    Public Shared blue_cs_bay5
+    Public Shared blue_cs_bay6
+    Public Shared blue_cs_bay7
+    Public Shared blue_cs_bay8
 
     'Data Sent from FMS Software to PLC
     Public Shared Match_Start 'MC13'
@@ -84,6 +129,10 @@ Public Class PLC_Handler
     Public Shared Field_Ready
     Public Shared Pre_Start_Match
     Public Shared PLC_Reset
+    Public Shared red_foul_count
+    Public Shared blue_foul_count
+    Public Shared red_tech_count
+    Public Shared blue_tech_count
 
     'Driver Station Statuses'
     Public Shared Red1Ready
@@ -169,15 +218,108 @@ Public Class PLC_Handler
     End Sub
 
     Public Shared Sub handleAutomatedScoring()
-        'TODO add scoring'
-
-        For i As Integer = 0 To red_hatch_count
+        'Updates the hatch points'
+        For Each red_hatch As Integer In red_hatch_count
             redHatchPanelPoints = redHatchPanelPoints + 2
         Next
 
-        For i As Integer = 0 To red_cargo_count
+        For Each blue_hatch As Integer In blue_hatch_count
+            blueHatchPanelPoints = blueHatchPanelPoints + 2
+        Next
+
+        'Updates the cargo points'
+        For Each red_cargo As Integer In red_cargo_count
             redCargoPoints = redCargoPoints + 3
         Next
+
+        For Each blue_cargo As Integer In blue_cargo_count
+            blueCargoPoints = blueCargoPoints + 3
+        Next
+
+        'Updates the fouls'
+        For Each red_fouls As Integer In red_foul_count
+            blueFoulPoints = blueFoulPoints + 3
+        Next
+
+        For Each blue_fouls As Integer In blue_foul_count
+            redFoulPoints = redFoulPoints + 3
+        Next
+
+        'Updates the tech fouls'
+        For Each red_tech_foul As Integer In red_tech_count
+            blueFoulPoints = blueFoulPoints + 10
+        Next
+
+        For Each blue_tech_foul As Integer In blue_tech_count
+            redFoulPoints = redFoulPoints + 10
+        Next
+
+        'Updates the foul counts'
+        redFoulCount = red_foul_count
+        redTechCount = red_tech_count
+        blueFoulCount = blue_foul_count
+        blueTechCount = blue_tech_count
+
+        'Red Rocket Placement Scoring'
+        If red_rocket_far_bay1 = 0 Then
+            redLowLeftRocketFar = "None"
+        ElseIf red_rocket_far_bay1 = 1 Then
+            redLowLeftRocketFar = "Panel"
+        ElseIf red_rocket_far_bay1 = 2 Then
+            redLowLeftRocketFar = "PanelAndCargo"
+        Else
+            redLowLeftRocketFar = "None"
+        End If
+
+        If red_rocket_far_bay2 = 0 Then
+            redLowRightRocketFar = "None"
+        ElseIf red_rocket_far_bay2 = 1 Then
+            redLowRightRocketFar = "Panel"
+        ElseIf red_rocket_far_bay2 = 2 Then
+            redLowRightRocketFar = "PanelAndCargo"
+        Else
+            redLowRightRocketFar = "None"
+        End If
+
+        If red_rocket_far_bay3 = 0 Then
+            redMidLeftRocketFar = "None"
+        ElseIf red_rocket_far_bay3 = 1 Then
+            redMidLeftRocketFar = "Panel"
+        ElseIf red_rocket_far_bay3 = 2 Then
+            redMidLeftRocketFar = "PanelAndCargo"
+        Else
+            redMidLeftRocketFar = "None"
+        End If
+
+        If red_rocket_far_bay4 = 0 Then
+            redMidRightRocketFar = "None"
+        ElseIf red_rocket_far_bay4 = 1 Then
+            redMidRightRocketFar = "Panel"
+        ElseIf red_rocket_far_bay4 = 2 Then
+            redMidRightRocketFar = "PanelAndCargo"
+        Else
+            redMidRightRocketFar = "None"
+        End If
+
+        If red_rocket_far_bay5 = 0 Then
+            redTopLeftRocketFar = "None"
+        ElseIf red_rocket_far_bay5 = 1 Then
+            redTopLeftRocketFar = "Panel"
+        ElseIf red_rocket_far_bay5 = 2 Then
+            redTopLeftRocketFar = "PanelAndCargo"
+        Else
+            redTopLeftRocketFar = "None"
+        End If
+
+        If red_rocket_far_bay6 = 0 Then
+            redMidRightRocketFar = "None"
+        ElseIf red_rocket_far_bay6 = 1 Then
+            redMidRightRocketFar = "Panel"
+        ElseIf red_rocket_far_bay6 = 2 Then
+            redMidRightRocketFar = "PanelAndCargo"
+        Else
+            redMidRightRocketFar = "None"
+        End If
 
     End Sub
 
@@ -290,8 +432,59 @@ Public Class PLC_Handler
     Public Shared Sub readRegisters()
         'This reads registers from the plc'
         Dim readRegisters() As Integer = modbusClient.ReadHoldingRegisters(0, 200)
+        'Hatch and Cargo Counts'
         red_hatch_count = readRegisters(0)
         red_cargo_count = readRegisters(1)
-
+        blue_hatch_count = readRegisters(2)
+        blue_cargo_count = readRegisters(3)
+        'Foul Counts'
+        red_foul_count = readRegisters(4)
+        red_tech_count = readRegisters(5)
+        blue_foul_count = readRegisters(6)
+        blue_tech_count = readRegisters(7)
+        'Red Rocket Placement'
+        red_rocket_far_bay1 = readRegisters(8)
+        red_rocket_far_bay2 = readRegisters(9)
+        red_rocket_far_bay3 = readRegisters(10)
+        red_rocket_far_bay4 = readRegisters(11)
+        red_rocket_far_bay5 = readRegisters(12)
+        red_rocket_far_bay6 = readRegisters(13)
+        red_rocket_near_bay1 = readRegisters(14)
+        red_rocket_near_bay2 = readRegisters(15)
+        red_rocket_near_bay3 = readRegisters(16)
+        red_rocket_near_bay4 = readRegisters(17)
+        red_rocket_near_bay5 = readRegisters(18)
+        red_rocket_near_bay6 = readRegisters(19)
+        'Red Cargoship Placement'
+        red_cs_bay1 = readRegisters(20)
+        red_cs_bay2 = readRegisters(21)
+        red_cs_bay3 = readRegisters(22)
+        red_cs_bay4 = readRegisters(23)
+        red_cs_bay5 = readRegisters(24)
+        red_cs_bay6 = readRegisters(25)
+        red_cs_bay7 = readRegisters(26)
+        red_cs_bay8 = readRegisters(27)
+        'Blue Rocket Placement'
+        blue_rocket_far_bay1 = readRegisters(28)
+        blue_rocket_far_bay2 = readRegisters(29)
+        blue_rocket_far_bay3 = readRegisters(30)
+        blue_rocket_far_bay4 = readRegisters(31)
+        blue_rocket_far_bay5 = readRegisters(32)
+        blue_rocket_far_bay6 = readRegisters(33)
+        blue_rocket_near_bay1 = readRegisters(34)
+        blue_rocket_near_bay2 = readRegisters(35)
+        blue_rocket_near_bay3 = readRegisters(36)
+        blue_rocket_near_bay4 = readRegisters(37)
+        blue_rocket_near_bay5 = readRegisters(38)
+        blue_rocket_near_bay6 = readRegisters(39)
+        'Blue Cargoship Placement'
+        blue_cs_bay1 = readRegisters(40)
+        blue_cs_bay2 = readRegisters(41)
+        blue_cs_bay3 = readRegisters(42)
+        blue_cs_bay4 = readRegisters(43)
+        blue_cs_bay5 = readRegisters(44)
+        blue_cs_bay6 = readRegisters(45)
+        blue_cs_bay7 = readRegisters(46)
+        blue_cs_bay8 = readRegisters(47)
     End Sub
 End Class
